4d3751ab4a80e27476b9a1375a2f981d4291f34a
= <=>
2025-08-03 12:26:35 +0530

    fixed global styles issue

--- src/provider/layout.provider.tsx
+++ src/provider/layout.provider.tsx
@@ -13,18 +13,24 @@ import ColorfulLayout from "@/layout/colorful-theme.layout";
 // theme
 import lightTheme from "@/theme/light.theme";
 import darkTheme from "@/theme/dark.theme";
+import colorfulTheme from "@/theme/colorful.theme";
 
 // context
 import { ModeContext } from "@/context";
 
 const GlobalStyle = createGlobalStyle<{
-  current_mode: IMode;
+  $current_mode: IMode;
 }>`
   body {
-    background-color:${(props) =>
-      props.current_mode == "dark"
-        ? darkTheme.palette.primary.main
-        : lightTheme.palette.primary.main};
+    background-color:${(props) => {
+      if (props.$current_mode == "dark") {
+        return darkTheme.palette.primary.main;
+      } else if (props.$current_mode == "light") {
+        return lightTheme.palette.primary.main;
+      } else {
+        return colorfulTheme.palette.primary.main;
+      }
+    }}
   }
 `;
 const layouts: Record<IMode, ReactElement> = {
@@ -47,7 +53,7 @@ const LayoutProvider: FC = () => {
         modeUpdater: (val) => setMode(val),
       }}
     >
-      <GlobalStyle current_mode={mode} />
+      <GlobalStyle $current_mode={mode} />
       {layouts[mode] ?? <LightLayout />}
     </ModeContext.Provider>
   );
