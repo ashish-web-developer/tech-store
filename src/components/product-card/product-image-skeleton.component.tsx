297ff5fcaa9f192f19449b3928523fabf286d140
= <=>
2025-08-03 09:22:45 +0530

    fixed skeleton size related issue

--- src/components/product-card/product-image-skeleton.component.tsx
+++ src/components/product-card/product-image-skeleton.component.tsx
@@ -5,7 +5,7 @@ const ProductImageSkeleton: FC = () => {
   return (
     <div
       role="status"
-      className="flex items-center justify-center h-56 max-w-sm bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700"
+      className="flex items-center justify-center w-full h-52  bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700"
     >
       <svg
         className="w-10 h-10 text-gray-200 dark:text-gray-600"
